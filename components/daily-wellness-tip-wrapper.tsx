1eb1d679d3951ceafdd71caaaf3827d70c0f0889
hamboi772 <abiodunhamboihameed@gmail.com>
2026-03-29 23:23:03 +0100

    Update daily-wellness-tip-wrapper.tsx

--- components/daily-wellness-tip-wrapper.tsx
+++ components/daily-wellness-tip-wrapper.tsx
@@ -1,6 +1,8 @@
+"use client"
+
 import dynamic from "next/dynamic"
 
 export const DailyWellnessTipWrapper = dynamic(
-  () => import("@/components/did-you-know-card").then((mod) => ({ default: mod.default })),
-  { ssr: false },
+  () => import("@/components/did-you-know-card"),
+  { ssr: false }
 )
