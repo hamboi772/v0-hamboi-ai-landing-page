1e097b190b995ed5eea9239996dc7786164a8529
hamboi772 <abiodunhamboihameed@gmail.com>
2026-06-07 19:25:10 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,16 +6,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [{ type: 'host', value: 'www.hamboimindcare.site' }],
-        destination: 'https://hamboimindcare.site/:path*',
-        permanent: true,
-      },
-    ]
-  },
 }
 
 export default nextConfig
